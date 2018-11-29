d2c47a072220c13deccbdea2063578569a527211
Joshua Schmidlkofer <joshland@gmail.com>
2018-11-28 19:22:04 -0800

    Update: fix default theme, better CLI support

--- emacs_text/files/CentOS-6.el
+++ emacs_text/files/CentOS-6.el
@@ -14,7 +14,7 @@
 
 (custom-set-variables
 '(ansi-color-faces-vector [default bold shadow italic underline bold bold-italic bold])
-'(custom-enabled-themes (quote (twilight-bright)))
+'(custom-enabled-themes (quote (sanityinc-tomorrow-bright)))
 '(custom-safe-themes (quote ("03f28a4e25d3ce7e8826b0a67441826c744cbf47077fb5bc9ddb18afe115005f" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" default)))
 '(vc-annotate-background nil)
 '(vc-annotate-color-map (quote ((20 . "#d54e53") (40 . "#e78c45") (60 . "#e7c547") (80 . "#b9ca4a") (100 . "#70c0b1") (120 . "#7aa6da") (140 . "#c397d8") (160 . "#d54e53") (180 . "#e78c45") (200 . "#e7c547") (220 . "#b9ca4a") (240 . "#70c0b1") (260 . "#7aa6da") (280 . "#c397d8") (300 . "#d54e53") (320 . "#e78c45") (340 . "#e7c547") (360 . "#b9ca4a"))))
